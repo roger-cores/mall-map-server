dd3e03024cca515c46869eede7fa9a0afb798775
roger-cores <rcorrei1@binghamton.edu>
2019-04-24 17:08:34 -0400

    cleanup - app.js

--- app.js
+++ app.js
@@ -19,61 +19,15 @@ var utils = require('./utils.js');
 var inputProductData = require('./inputProductData');
 var inputLinkData = require('./inputLinkData');
 
-
-
-// var trainingDatabase = new Sequelize(`postgres://${process.env.userid?process.env.userid:'postgres'}:${process.env.passwd?process.env.passwd:'pass'}@${process.env.dburi?process.env.dburi:'localhost'}:${process.env.dbport?process.env.dbport:'5432'}/${process.env.dbname?process.env.dbname:'mallmap'}`);
 var mapsDatabase = new Sequelize(`postgres://${process.env.userid?process.env.userid:'postgres'}:${process.env.passwd?process.env.passwd:'pass'}@${process.env.dburi?process.env.dburi:'localhost'}:${process.env.dbport?process.env.dbport:'5432'}/${process.env.mapsdbname?process.env.mapsdbname:'maps'}`);
-// var trainingDatabase = new Sequelize('postgres://wjvzstiabdqjec:22970eb9b5a299456638ed9bf594458b809b71f7ed5a0656346be7acb7e31e4f@ec2-54-246-108-119.eu-west-1.compute.amazonaws.com:5432/df29fe95b26a5a');
-// var TrainingSet = models.TrainingSet(trainingDatabase, Sequelize);
-// var ClassRecord = models.ClassRecord(trainingDatabase, Sequelize);
-// var Beacon = models.Beacon(trainingDatabase, Sequelize);
-// var Link = models.Link(trainingDatabase, Sequelize);
-// var Product = models.Product(trainingDatabase, Sequelize);
 var MapImage = models.MapImage(mapsDatabase, Sequelize);
-
-// var User = models.User(trainingDatabase, Sequelize);
-// User.sync();
-// inputProductData(Product);
-
-// ClassRecord.hasMany(TrainingSet);
-// TrainingSet.belongsTo(ClassRecord);
-//
-// Link.belongsTo(ClassRecord, {
-//   as: 'sourceLabel',
-//   foreignKey: 'source_label'
-// });
-//
-// Link.belongsTo(ClassRecord, {
-//   as: 'destinationLabel',
-//   foreignKey: 'destination_label'
-// });
-//
-// Product.belongsTo(ClassRecord, {
-//   as: 'classLabel',
-//   foreignKey: 'class_label'
-// });
-
-
-// ClassRecord.sync();
-// TrainingSet.sync();
-// Beacon.sync();
-// Link.sync();
-// Product.sync();
 MapImage.sync();
 
-// inputLinkData(Link);
-
 var app = express();
-
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'hbs');
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
